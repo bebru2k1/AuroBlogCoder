bd91a24d088ded747d288125d021e6f0c223a58f
bebru2k1 <66669380+bebru2k1@users.noreply.github.com>
2021-09-05 21:12:45 +0700

    Update axiosConfig.js

--- my-app/src/configs/axiosConfig.js
+++ my-app/src/configs/axiosConfig.js
@@ -1,7 +1,9 @@
 import axios from "axios"
 
 const instance = axios.create({
-    baseURL: 'http://localhost:5000/v1/api'
+    baseURL: process.env.NODE_ENV !== 'production'
+        ? 'http://localhost:5000/v1/api'
+        : 'https://infinite-reaches-00896.herokuapp.com/v1/api'
 })
 
 export default instance
